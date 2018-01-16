1c9b4113d61cd187007cceda8d0e59458b03172e
jittagornp <jittagornp@gmail.com>
2018-01-16 18:37:33 +0700

    Can connect database

--- src/main/java/com/paigunna/AppStarter.java
+++ src/main/java/com/paigunna/AppStarter.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.paigunna.web;
+package com.paigunna;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
